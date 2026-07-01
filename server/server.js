7ec1f121a352b129d431439a9b7f6c9e80c338fc
NXL <linhdenday16206@gmail.com>
2026-07-01 20:09:59 +0700

    Khởi tạo kiến trúc client server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const PORT = process.env.PORT;
+
+app.get('/api/products', (req, res) => {
+    res.json([
+        { id: 1, name: "Áo sơ mi Shop Web", price: 250000 },
+        { id: 2, name: "Quần Jean Express", price: 350000 }
+    ]);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server đang chạy tại http://localhost:${PORT}`);
+});
